729392c1412ef655b0523e16934eb371426a29a1
adr1an0s0ar3s <adrianosoares35@gmail.com>
2021-11-19 18:57:11 +0000

    :pencil2: Added Fib.hs

---
+++ Fib.hs
@@ -0,0 +1,24 @@
+----------------------- 1.1 -----------------------
+
+fibRec :: (Integral a) => a -> a
+fibRec 0 = 0
+fibRec 1 = 1
+fibRec x = fibRec (x-1) + fibRec (x-2)
+
+----------------------- 1.2 -----------------------
+
+fibLista :: (Integral a) => a -> a
+fibLista x = fibListaAux x [0,1]
+
+fibListaAux :: (Integral a) => a -> [a] -> a
+fibListaAux 0 [x,y] = x
+fibListaAux 1 [x,y] = y
+fibListaAux a [x,y] = fibListaAux (a-1) [y,x+y]
+
+----------------------- 1.3 -----------------------
+
+fibListaInfinita :: (Integral a) => a -> a
+fibListaInfinita x = fib !! fromIntegral x
+
+fib :: (Integral a) => [a]
+fib = 0 : 1 : [n | x <- [2..], let n = fib !! (x-1) + fib !! (x-2)]
