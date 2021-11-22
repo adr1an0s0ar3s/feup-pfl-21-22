37430ace49bc28d2ad7db26971081dc9ba53a131
adr1an0s0ar3s <adrianosoares35@gmail.com>
2021-11-22 08:31:21 +0000

    :pencil2: Added some functions to BigNumber.hs that may be easier to read

--- BigNumber.hs
+++ BigNumber.hs
@@ -8,14 +8,26 @@ type BigNumber = [Int]
 
 ----------------------- 2.2 -----------------------
 
-scanner :: String -> BigNumber 
+scanner :: String -> BigNumber
 scanner s = if head s == '-' then [- digitToInt x | x <- take 1 s'] ++ [digitToInt x | x <- tail s']
             else [digitToInt x | x <- s]
             where s' = tail s
 
+scanner' :: String -> BigNumber
+scanner' []         = []
+scanner' [a]        = [digitToInt a]
+scanner' (a:b:cs)   | a == '-'  = (- digitToInt b) : [digitToInt x | x <- cs]
+                    | otherwise = [digitToInt x | x <- a:b:cs]
+
 ----------------------- 2.3 -----------------------
 
-output :: BigNumber  -> String
+output :: BigNumber -> String
 output bignumber = if head bignumber < 0 then "-" ++ bignumber'
                    else [intToDigit x | x <- bignumber]
                    where bignumber' = [intToDigit (abs x) | x <- bignumber]
+
+output' :: BigNumber -> String
+output' [] = []
+output' (x:xs)  | x < 0 = '-' : bignumber
+                | otherwise = bignumber
+                where bignumber = [intToDigit (abs a) | a <- x:xs]
