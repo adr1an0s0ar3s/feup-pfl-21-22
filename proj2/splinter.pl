c94a95f38a9a619d9d17e51dbd21ea50c21cb68a
adr1an0s0ar3s <adrianosoares35@gmail.com>
2022-01-09 22:28:18 +0000

    :pencil2: Cleaned and documented code, utils.pl file added, diagonal set and get are now functional!

--- proj2/splinter.pl
+++ proj2/splinter.pl
@@ -1,3 +1,4 @@
+:- [utils].
 
 /*
 Tabuleiro de jogo, 18x15, é iniciado com as seguintes peças ao centro:
@@ -47,7 +48,8 @@ move([L | R], X, Y, 'E', [L | NewR]) :-
     move(R, X, NewY, 'E', NewR).
 
 move([L | R], X, 0, 'O', [NewL | R]) :-
-    NewX is 2 - X, % Pass length as argument!
+    length(L, A1),
+    NewX is A1 - 1 - X,
     revert(L, L1),
     move_right(L1, NewX, L2),
     revert(L2, NewL).
@@ -56,125 +58,11 @@ move([L | R], X, Y, 'O', [L | NewR]) :-
     move(R, X, NewY, 'O', NewR).
 
 move(Board, X, Y, 'S', NewBoard) :-
-    transpose(Board, 3, 3, B1),
+    transpose(Board, B1),
     move(B1, Y, X, 'E', B2),
-    transpose(B2, 3, 3, NewBoard).
+    transpose(B2, NewBoard).
 
 move(Board, X, Y, 'N', NewBoard) :-
-    transpose(Board, 3, 3, B1),
+    transpose(Board, B1),
     move(B1, Y, X, 'O', B2),
-    transpose(B2, 3, 3, NewBoard).
-
-
-
-
-
-
-
-/*
-Removes the first instance of the first argument in the list of the second argument
-resulting in the list present in the third argument.
-*/
-del_elem(A1, [A1 | T], T) :- !.
-del_elem(A1, [H | T1], [H | T2]) :- del_elem(A1, T1, T2).
-
-/*
-Reverts the list in the first argument resulting in the list present in the second
-argument.
-*/
-revert([], []).
-revert([H | T], L2) :-
-    revert(T, L1),
-    append(L1, [H], L2).
-
-
-get_elem(0, [E | _], E).
-get_elem(I, [_ | T], E) :-
-    I > 0,
-    NewI is I - 1,
-    get_elem(NewI, T, E).
-
-set_elem(0, E, [_ | T], [E | T]).
-set_elem(I, E, [H | T], [H | NewT]) :-
-    I > 0,
-    NewI is I - 1,
-    set_elem(NewI, E, T, NewT).
-
-get_column(X, Height, Matrix, C) :- get_column(X, Height, Matrix, [], C).
-get_column(_, 0, _, R, R).
-get_column(X, Height, [L | R], Acc1, C) :-
-    Height > 0,
-    get_elem(X, L, A1),
-    append(Acc1, [A1], Acc2),
-    NewHeight is Height - 1,
-    get_column(X, NewHeight, R, Acc2, C).
-
-transpose(Matrix, Length, Height, NewMatrix) :- transpose(Matrix, Length, Height, [], NewMatrix).
-transpose(_, 0, _, R, R).
-transpose(Matrix, Length, Height, Acc1, NewMatrix) :-
-    Length > 0,
-    X is Length - 1,
-    get_column(X, Height, Matrix, L1),
-    append([L1], Acc1, Acc2),
-    NewLength is Length - 1,
-    transpose(Matrix, NewLength, Height, Acc2, NewMatrix).
-
-
-get_matrix_elem(Matrix, X, Y, E) :-
-    get_elem(Y, Matrix, L),
-    get_elem(X, L, E).
-
-set_matrix_elem([L | T], X, 0, E, [NewL | T]) :-
-    set_elem(X, E, L, NewL).
-set_matrix_elem([L | R], X, Y, E, [L | NewR]) :-
-    NewY is Y - 1,
-    set_matrix_elem(R, X, NewY, E, NewR).
-
-get_diagonal(Matrix, X, Y, Length, Height, Diagonal) :-
-    X >= Y,
-    NewX is X - Y,
-    get_diagonal_aux(Matrix, NewX, 0, Length, Height, Diagonal).
-get_diagonal(Matrix, X, Y, Length, Height, Diagonal) :-
-    X < Y,
-    NewY is Y - X,
-    get_diagonal_aux(Matrix, 0, NewY, Length, Height, Diagonal).
-
-get_diagonal_aux(Matrix, X, Y, Length, Height, Diagonal) :- get_diagonal_aux(Matrix, X, Y, Length, Height, [], Diagonal).
-get_diagonal_aux(_, X, _, X, _, R, R).
-get_diagonal_aux(_, _, Y, _, Y, R, R).
-get_diagonal_aux(Matrix, X, Y, Length, Height, Acc1, Diagonal) :-
-    X < Length,
-    Y < Height,
-    get_matrix_elem(Matrix, X, Y, E),
-    append(Acc1, [E], Acc2),
-    NewX is X + 1,
-    NewY is Y + 1,
-    get_diagonal_aux(Matrix, NewX, NewY, Length, Height, Acc2, Diagonal).
-
-set_diagonal_aux(Matrix, X, Y, Length, Height, Diagonal, NewMatrix) :- set_diagonal_aux(Matrix, X, Y, Length, Height, Diagonal, [], NewMatrix).
-set_diagonal_aux(_, X, _, X, _, R, R).
-set_diagonal_aux(_, _, Y, _, Y, R, R).
-set_diagonal_aux(Matrix, X, Y, Length, Height, Diagonal, Acc1, NewMatrix) :-
-    X < Length,
-    Y < Height,
-    set_matrix_elem(Matrix, X, Y, E, NewMatrix),
-    append(Acc1, [E], Acc2),
-    NewX is X + 1,
-    NewY is Y + 1,
-    set_diagonal_aux(Matrix, NewX, NewY, Length, Height, Diagonal, Acc2, NewMatrix).
-
-/*
-[empty, empty, empty, empty, empty, empty, empty]
-[empty, empty, empty, empty, empty, empty, empty]
-[empty, empty, empty, empty, empty, >HERE, empty]
-[empty, empty, empty, empty, empty, empty, empty]
-[empty, empty, empty, empty, empty, empty, empty]
-[empty, empty, empty, empty, empty, empty, empty]
-[empty, empty, empty, empty, empty, empty, empty]
-
-Board Size: 7x7
-(5,2) -> (3,0)
-x > y -> x - y
-x < y -> y - x
-(3,0) -> (4,1) -> (5,2) -> (6,3) -> (7,4) X
-*/
+    transpose(B2, NewBoard).
---
+++ proj2/utils.pl
@@ -0,0 +1,103 @@
+/*
+Removes the first instance of the first argument in the list of the second argument
+resulting in the list present in the third argument.
+*/
+del_elem(A1, [A1 | T], T) :- !.
+del_elem(A1, [H | T1], [H | T2]) :- del_elem(A1, T1, T2).
+
+/*
+Reverts the list in the first argument resulting in the list present in the second
+argument.
+*/
+revert([], []).
+revert([H | T], L2) :-
+    revert(T, L1),
+    append(L1, [H], L2).
+
+/*
+Retrieves to the third argument the element present in the index given in the first
+argument in the list presented in the second argument.
+*/
+get_elem(0, [E | _], E).
+get_elem(I, [_ | T], E) :-
+    I > 0,
+    NewI is I - 1,
+    get_elem(NewI, T, E).
+
+/*
+Sets the element of index given in the first argument of the list given in the third
+argument to the value presented in the second argument, the resultant list is presented
+in the fourth argument.
+*/
+set_elem(0, E, [_ | T], [E | T]).
+set_elem(I, E, [H | T], [H | NewT]) :-
+    I > 0,
+    NewI is I - 1,
+    set_elem(NewI, E, T, NewT).
+
+/*
+Obtains the column of index given in the second argument of the matrix presented in
+the first argument resulting in the list in the third argument.
+*/
+get_column(Matrix, X, C) :- get_column(Matrix, X, [], C).
+get_column([], _, R, R).
+get_column([L | R], X, Acc1, C) :-
+    get_elem(X, L, A1),
+    append(Acc1, [A1], Acc2),
+    get_column(R, X, Acc2, C).
+
+/*
+Tranposes a matrix given in the first argument, the result is presented in the second
+argument.
+*/
+transpose(Matrix, NewMatrix) :- transpose(Matrix, 0, [], NewMatrix).
+transpose([L | _], X, R, R) :-
+    length(L, X).
+transpose(Matrix, X, Acc1, NewMatrix) :-
+    NewX is X + 1,
+    get_column(Matrix, X, L1),
+    append(Acc1, [L1], Acc2),
+    transpose(Matrix, NewX, Acc2, NewMatrix).
+
+/*
+Obtains the Beta 2/4 diagonal of the matrix presented in the first argument that includes
+the element of the coordinates given in the second and third argument, the result is
+returned by the fourth argument.
+*/
+get_diagonal(Matrix, X, Y, Diagonal) :-
+    X >= Y,
+    NewX is X - Y,
+    get_diagonal_aux(Matrix, NewX, [], Diagonal).
+get_diagonal([_ | R], X, Y, Diagonal) :-
+    X < Y,
+    NewY is Y - 1,
+    get_diagonal(R, X, NewY, Diagonal).
+get_diagonal_aux([], _, R, R).
+get_diagonal_aux([L | _], X, R, R) :-
+    length(L, X).
+get_diagonal_aux([L | R], X, Acc1, Diagonal) :-
+    NewX is X + 1,
+    get_elem(X, L, A1),
+    append(Acc1, [A1], Acc2),
+    get_diagonal_aux(R, NewX, Acc2, Diagonal).
+
+/*
+Sets the Beta 2/4 diagonal of the matrix presented in the first argument that includes
+the element of the coordinates given in the second and third argument with the list of
+values given in the fourth argument, the resultant matrix is returned by the fourth argument.
+*/
+set_diagonal(Matrix, X, Y, Diagonal, NewMatrix) :-
+    X >= Y,
+    NewX is X - Y,
+    set_diagonal_aux(Matrix, NewX, Diagonal, NewMatrix).
+set_diagonal([L | R], X, Y, Diagonal, [L | NewR]) :-
+    X < Y,
+    NewY is Y - 1,
+    set_diagonal(R, X, NewY, Diagonal, NewR).
+set_diagonal_aux([], _, [], []).
+set_diagonal_aux([L | _], X, [], []) :-
+    length(L, X).
+set_diagonal_aux([L | R], X, [H | T], [NewL | NewR]) :-
+    NewX is X + 1,
+    set_elem(X, H, L, NewL),
+    set_diagonal_aux(R, NewX, T, NewR).
