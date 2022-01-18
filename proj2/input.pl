526650d2fa030e9bdf9923528a9d778f7d2e436a
Vasco52 <64026324+Vasco52@users.noreply.github.com>
2022-01-18 14:53:37 +0000

    User input is now being saved properly

--- proj2/input.pl
+++ proj2/input.pl
@@ -1,6 +1,8 @@
 :- [utils].
 
 read_position(Column, Row) :-
+    repeat,
+    write('Please select a valid position (Column-Row):\n'),
     read(ColumnLetter-Row),
     char_to_number(ColumnLetter, Column),
     validate_position(Column, Row).
@@ -8,12 +10,10 @@ read_position(Column, Row) :-
 validate_position(Column, Row) :- 
     Column =< 14, Column >= 0,
     Row =< 17, Row >= 0.
-validate_position(Column, Row) :-
-    write('Please select a valid position (Column-Row):\n'),
-    read_position(NewColumn, NewRow),
-    validate_position(NewColumn, NewRow).
 
 read_direction(Direction) :-
+    repeat,
+    write('Please select a valid direction:\n'),
     read(Direction),
     validate_direction(Direction).
 
@@ -25,14 +25,8 @@ validate_direction(ne).
 validate_direction(no).
 validate_direction(se).
 validate_direction(so).
-validate_direction(_Direction) :- 
-    write('Please select a valid direction:\n'),
-    read(NewDirection),
-    validate_direction(NewDirection).
 
-read_move(Column, Row, Direction) :- % TODO: the first input is the one being saved, despite being correct or not.
-    write('Please select the position (Column-Row):\n'),
+read_move(Column, Row, Direction) :- 
     read_position(Column, Row),
-    write('Please select the direction:\n'),
     read_direction(Direction).
 
